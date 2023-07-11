941b1d301ce215e2049b33ccd58c89347f555970
irostub <irostub@gmail.com>
2023-07-11 13:18:47 +0900

    Sort 적용

--- src/main/java/com/kubenews/koogleapiserver/NewsDataApiController.java
+++ src/main/java/com/kubenews/koogleapiserver/NewsDataApiController.java
@@ -3,6 +3,7 @@ package com.kubenews.koogleapiserver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +21,9 @@ public class NewsDataApiController {
     @GetMapping("/list")
     public ResponseEntity<PagingResponse<List<KoogleNews>>> getNewsData(@RequestParam(required = false, defaultValue = "1") Integer page,
                                                                         @RequestParam(required = false, defaultValue = "10") Integer size,
-                                                                        @RequestParam(required = false, defaultValue = "") String keyword) {
-        PageRequest pageable = PageRequest.of(page - 1, size);
+                                                                        @RequestParam(required = false, defaultValue = "") String keyword,
+                                                                        @RequestParam(required = false, defaultValue = "publishDate") String sort) {
+        PageRequest pageable = PageRequest.of(page - 1, size, Sort.by(sort).descending());
         Page<KoogleNews> koogleNewsPage = repository.findPageByContentContains(pageable, keyword);
         PagingResponse<List<KoogleNews>> pagingResponse = PagingResponse.success(koogleNewsPage);
         return ResponseEntity.ok(pagingResponse);
